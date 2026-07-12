4faf65769e0c2f4e65c96c652136d8a59fae2561
Cleiton Aleixo da Silva <cleitn1@gmail.com>
2026-07-12 00:17:50 -0300

    inclui variaveis do terraform

---
+++ infra/dev/vars.tfvars
@@ -0,0 +1,6 @@
+
+bucket          = "tse-datalake-portifolio"
+lambda_function = "tse-data-ingestion-lambda"
+ecr_repository  = "tse-data-ingestion-ecr"
+timeout         = 900
+memory_size     = 1024
---
+++ infra/vars.tf
@@ -0,0 +1,13 @@
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "bucket" {}
+variable "lambda_function" {}
+variable "lambda_handler" {}
+variable "runtime" {}
+variable "cron" {}
+variable "timeout" {}
+variable "memory_size" {}
+variable "ecr_repository_name" {}
